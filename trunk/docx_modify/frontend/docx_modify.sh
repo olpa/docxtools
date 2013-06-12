5200e7d8e95eeb8b438b504ad368fae6e228932d
gimsieke <gimsieke@e9a79386-55cf-4f64-99e8-d459d4b3665a>
2013-06-12 11:17:37 +0000

    front end script, documentation

---
+++ trunk/docx_modify/frontend/docx_modify.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cygwin=false;
+case "`uname`" in
+  CYGWIN*) cygwin=true;
+esac
+
+DIR="$( cd -P "$(dirname $( readlink -f "${BASH_SOURCE[0]}" ))" && pwd )"
+XSL="$( readlink -f "$1" )"
+DOCX="$( readlink -f "$2" )"
+XPL="$DIR"/lib/xpl/docx_modify.xpl
+
+if [ -z $DEBUG ]; then
+    DEBUG=no
+fi
+
+if [ -z $XSL ]; then
+    echo "Please supply an .xsl file as first argument (e.g., $DIR/lib/xsl/identity.xsl)"
+    exit 1
+fi
+
+if [ -z $DOCX ]; then
+    echo "Please supply a .docx file as second argument"
+    exit 1
+fi
+
+if $cygwin; then
+  XSL=file:/$(cygpath -ma $XSL)
+  DOCX=$(cygpath -ma $DOCX)
+  XPL=file:/$(cygpath -ma $XPL)
+fi
+
+$DIR/calabash/calabash.sh -i xslt="$XSL" "$XPL" file="$DOCX" debug=$DEBUG
