a8d651f0015fc2d8a23bd4cfd533139d0b30c851
pglatza <pglatza@e9a79386-55cf-4f64-99e8-d459d4b3665a>
2013-07-30 09:52:51 +0000

    added usage sample

--- trunk/docx_modify/frontend/docx_modify.sh
+++ trunk/docx_modify/frontend/docx_modify.sh
@@ -7,7 +7,20 @@ esac
 DIR="$( cd -P "$(dirname $( readlink -f "${BASH_SOURCE[0]}" ))" && pwd )"
 XSL="$( readlink -f "$1" )"
 DOCX="$( readlink -f "$2" )"
+DEBUG="$( readlink -f "$3" )"
+HEAP="$( readlink -f "$4" )"
 XPL="$DIR"/lib/xpl/docx_modify.xpl
+
+if [ -z $XSL ]; then
+    echo "Usage: docx_modifiy XSL DOCX [DEBUG] [HEAP]";
+    echo "Sample invocation (identity): ./docx_modifiy.sh lib/xsl/identity.xsl /path/to/myfile.docx yes";
+    exit 1;
+fi
+
+if [ -z $DOCX ]; then
+    echo "Please supply a .docx file as second argument"
+    exit 1
+fi
 
 if [ -z $DEBUG ]; then
     DEBUG=no
@@ -15,16 +28,6 @@ fi
 
 if [ -z $HEAP ]; then
     HEAP=1024m
-fi
-
-if [ -z $XSL ]; then
-    echo "Please supply an .xsl file as first argument (e.g., $DIR/lib/xsl/identity.xsl)"
-    exit 1
-fi
-
-if [ -z $DOCX ]; then
-    echo "Please supply a .docx file as second argument"
-    exit 1
 fi
 
 if $cygwin; then
