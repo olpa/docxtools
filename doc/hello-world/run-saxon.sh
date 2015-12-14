0e254b361140512289bf2a03669ec014f8a4305b
Oleg Parashchenko <olpa@uucode.com>
2015-12-14 09:21:56 +0100

    Path to the collection file should be absolute.

--- doc/hello-world/run-saxon.sh
+++ doc/hello-world/run-saxon.sh
@@ -1,8 +1,9 @@
 #
-INFILE=intermediate_not_matched.xml
+INFILE=5_not_matched.xml
 OUTFILE=document.xml
+COLLFILE=`pwd`/collection.xml # Absolute path required
 MODE=default # Second pass: clean, third: merge
 
-CODEDIR=../..
-java -cp $CODEDIR/calabash/resolver/resolver.jar:$CODEDIR/calabash/lib/saxon9he.jar net.sf.saxon.Transform -s:$INFILE --defaultCollection:collection.xml -o:$OUTFILE -xsl:$CODEDIR/hub2docx/lib/xsl/hub2docx.xsl -catalog:$CODEDIR/xslt-util/xmlcatalog/catalog.xml';'$CODEDIR/docx_modify/lib/xmlcatalog/catalog.xml -im:'{http://www.le-tex.de/namespace/hub}'$MODE
+CODEDIR=docxtools
+java -cp $CODEDIR/calabash/resolver/resolver.jar:$CODEDIR/calabash/lib/saxon9he.jar net.sf.saxon.Transform -s:$INFILE --defaultCollection:$COLLFILE -o:$OUTFILE -xsl:$CODEDIR/hub2docx/lib/xsl/hub2docx.xsl -catalog:$CODEDIR/xslt-util/xmlcatalog/catalog.xml';'$CODEDIR/docx_modify/lib/xmlcatalog/catalog.xml -im:'{http://www.le-tex.de/namespace/hub}'$MODE
 # -explain:explain.xml -opt:0
